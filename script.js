6aa55ce23e8c598391f0166b5245e28b991a9702
majakochanowska <51491969+majakochanowska@users.noreply.github.com>
2020-01-18 22:50:53 +0100

    fixed few issues in FF and IE

--- script.js
+++ script.js
@@ -2,35 +2,38 @@ $(function() {
   var storedEmployee = JSON.parse(localStorage.getItem("store-employee"));
   var storedCompany = JSON.parse(localStorage.getItem("store-company"));
 
-    var sources = {
-        'Company ABC' : [ "Employee 1", "Employee 2", "Employee 3" ],
-        'Company CDE' : [ "Employee 4", "Employee 5", "Employee 6" ],
-        'Company EFG' : [ "Employee 7", "Employee 8", "Employee 9" ],
-        'Company GHI' : [ "Employee 10", "Employee 11", "Employee 12" ]
+  var sources = {
+    'Company ABC' : [ "Employee 1", "Employee 2", "Employee 3" ],
+    'Company CDE' : [ "Employee 4", "Employee 5", "Employee 6" ],
+    'Company EFG' : [ "Employee 7", "Employee 8", "Employee 9" ],
+    'Company GHI' : [ "Employee 10", "Employee 11", "Employee 12" ]
+  }
+
+  $("#company").autocomplete({
+    source: Object.keys(sources),
+    change: function(event) {
+      $("#employee").autocomplete("option", {
+        source: sources[event.currentTarget.value]
+      });
+      localStorage.setItem("store-company", JSON.stringify(event.currentTarget.value));
+    },   
+  }).val(storedCompany);
+
+  $("#employee").autocomplete({
+    source: [],
+    change: function() {
+      localStorage.setItem("store-employee", JSON.stringify(event.currentTarget.value));
+      tableClear();
     }
+  }).val(storedEmployee);
 
-      $("#company").autocomplete({
-        source: Object.keys(sources),
-        change: event => {
-          $("#employee").autocomplete("option", {
-            source: sources[event.currentTarget.value]
-          });
-          localStorage.setItem("store-company", JSON.stringify(event.currentTarget.value));
-        },   
-      }).val(storedCompany);
-
-      $("#employee").autocomplete({
-        source: [],
-        change: function() {
-          localStorage.setItem("store-employee", JSON.stringify(event.currentTarget.value));
-          tableClear();
-        }
-      }).val(storedEmployee)
+  $("#addButton").click(tableUpdate);
 }) 
 
 var EXCHANGE = 4.8282;
 
 function addTask() {
+  console.log('table');
   if ($("#taskTable tbody").length == 0) {
     $("#taskTable").append("<tbody></tbody>");
   }
@@ -49,21 +52,23 @@ function addTask() {
 function tableUpdate() {
   if ($("#task").val() == "") {
     $("#task-error").text("Podaj nazwę zadania");
-    $("#task").css("border", "1px solid red")
+    $("#task").css("border", "1px solid red");
+    return false;
   }
   else if ($("#task").val().length < 5) {
     $("#task-error").text("Nazwa zadania musi mieć co najmniej 5 znaków");
-    $("#task").css("border", "1px solid red")
+    $("#task").css("border", "1px solid red");
+    return false;
   }
   else if ($("#amount").val() == "") {
     $("#amount-error").text("Podaj kwotę w PLN (liczba)");
-    $("#amount").css("border", "1px solid red")
+    $("#amount").css("border", "1px solid red");
+    return false;
   }
   else {
     removeErrorMessage();
     addTask();
     formClear();
-    $("#task").focus();
     calculateSum();
   }
 }
